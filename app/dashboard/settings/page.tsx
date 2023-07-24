c1a0abc2d43534c02d9db023868dfa74267e4345
qrowned <admin@qrowned.dev>
2023-07-24 13:53:16 +0200

    Add coming soon to settings page

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,7 @@
+export default async function SettingsPage() {
+  return (
+    <h1 className="font-extrabold bg-gradient-to-r from-red-500 to-indigo-600 bg-clip-text text-transparent text-4xl leading-tight">
+      Coming soon...
+    </h1>
+  )
+}
